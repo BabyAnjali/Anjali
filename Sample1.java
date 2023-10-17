6bfaa121bec163395cc370f5b52d445dfd0b1232
BabyAnjali <146073776+BabyAnjali@users.noreply.github.com>
2023-10-17 05:40:15 -0400

    Create Sample1.java

---
+++ Sample1.java
@@ -0,0 +1,16 @@
+class A
+{
+public static void main(String args[])
+{
+int a=10,int b=20,int c;
+System.out.println("Before swapping:");
+System.out.println("First number is:"+a);
+System.out.println("Second number is:"+b);
+c=a;
+a=b;
+b=c;
+System.out.println("After swapping:");
+System.out.println("a="+a);
+System.out.println("b="+b);
+}
+}
